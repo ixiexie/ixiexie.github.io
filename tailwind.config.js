7d9364305d3080c51a10aab83ec37ffd73bd3efe
mshu4 <123998895+ixiexie@users.noreply.github.com>
2023-11-22 04:06:07 -0600

    11

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['*.html','pages/*.html'],
+  content: ['./*.html'],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
